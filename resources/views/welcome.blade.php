0b8cfd7868872fc74fcac865acc363faeb09b796
Vlad Nicu <nicuvlad89@gmail.com>
2017-12-05 22:55:35 +0200

    changed the name of the app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CookBook
                 </div>
 
                 <div class="links">
